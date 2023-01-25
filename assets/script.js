6f68aebf3421236111e78c9d5b1388076559d019
mcquo011 <omcquown@gmail.com>
2023-01-24 18:35:32 -0600

    card placement and getting 10 recipes to display

--- assets/script.js
+++ assets/script.js
@@ -11,37 +11,51 @@ const API_key = "27e126a985485dda204f30a5e04158f8";
 function getRecipe() {
   let APIurl = `https://api.edamam.com/search?q=${searchQuery}&app_id=${API_id}&app_key=${API_key}&to=10`;
 
-  fetch(APIurl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const recipe = data.hits;
-      calories.innerHTML = "Calories: " + data.hits[0].recipe.calories;
-      title.innerHTML = data.hits[0].recipe.label;
-      nutrition.innerHTML = "Diet Label: " + data.hits[0].recipe.dietLabels;
-
-      document.querySelector(".image img").src = data.hits[0].recipe.image;
-
-      const ingredientsList = data.hits[0].recipe.ingredientLines;
-      const select = document.querySelector("select");
-
-
+fetch(APIurl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const recipes = data.hits;
+    for (let i = 0; i < recipes.length; i++) {
+      const cardTitle = document.querySelectorAll(".title")[i];
+      const cardCalories = document.querySelectorAll(".calories")[i];
+      const cardNutrition = document.querySelectorAll(".nutrition")[i]; 
+      const cardImage = document.querySelectorAll(".image img")[i];
+      const ingredientsList = data.hits[i].recipe.ingredientLines;
+      const select = document.querySelectorAll(".select")[i];
+      cardTitle.innerHTML = recipes[i].recipe.label;
+      cardCalories.innerHTML =
+     "Calories: " + Math.round(recipes[i].recipe.calories);
+        if (recipes[i].recipe.dietLabels.length > 0) {
+          cardNutrition.innerHTML =
+            "Diet Label: " + recipes[i].recipe.dietLabels;
+        } else {
+          cardNutrition.innerHTML = "Diet Label: No Data Found";
+        }
+      cardImage.src = recipes[i].recipe.image;
+      
       while (select.firstChild) {
         select.removeChild(select.firstChild);
       }
-
-      for (let i = 0; i < ingredientsList.length; i++) {
+      for (let j = 0; j < ingredientsList.length; j++) {
         const option = document.createElement("option");
-        option.innerHTML = ingredientsList[i];
+        option.innerHTML = ingredientsList[j];
         select.appendChild(option);
       }
-    })
-    .catch((error) => console.log(error));
+    }
+  })
+  .catch((error) => console.log(error));
 }
+
+// Hide all recipe-cards by default 
+const cards = document.querySelectorAll(".recipe-card");
+cards.forEach((card) => card.classList.add("hidden"));
 
 search.addEventListener("click", function (event) {
   event.preventDefault();
+  // Hide all cards
+  cards.forEach((card) => card.classList.add("hidden"));
   searchQuery = document.querySelector("input").value;
   getRecipe();
 });
