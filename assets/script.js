1e9807d7ac58ff6efafe0ac7ad76f97969c18d81
mcquo011 <omcquown@gmail.com>
2023-01-26 20:56:14 -0600

    delete this pull request

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,11 @@ const ingredients = document.querySelector(".ingredients");
 const image = document.querySelector(".image");
 const title = document.querySelector("#title");
 const nutrition = document.querySelector(".nutrition");
+const historyList = document.querySelector("#historyContainer");
 const API_id = "cf07f1f8";
 const API_key = "27e126a985485dda204f30a5e04158f8";
+
+let history = JSON.parse(localStorage.getItem("history")) || [];
 
 function getRecipe() {
   let APIurl = `https://api.edamam.com/search?q=${searchQuery}&app_id=${API_id}&app_key=${API_key}&to=10`;
@@ -62,7 +65,6 @@ fetch(APIurl)
   .catch((error) => console.log(error));
 }
 
-
 search.addEventListener("click", function (event) {
   event.preventDefault();
   // Hide all cards
@@ -70,4 +72,34 @@ search.addEventListener("click", function (event) {
   cards.forEach((card) => card.classList.remove("hidden"));
   searchQuery = document.querySelector("input").value;
   getRecipe();
+  //save search to localstorage
+  history.push(searchQuery);
+  localStorage.setItem("history", JSON.stringify(history));
 });
+
+function renderHistory(){
+     history = JSON.parse(localStorage.getItem("history")) || []; 
+    historyList.innerHTML = "";
+    for (let i = 0; i < history.length; i++) {
+        const historyBtn = document.createElement("button");
+        historyBtn.innerHTML = history[i]
+         historyBtn.setAttribute("type", "text");
+         historyBtn.setAttribute("value", searchHistory[i]);
+    }
+
+    historyList.appendChild(historyBtn);
+}
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  renderHistory();
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  
+}
